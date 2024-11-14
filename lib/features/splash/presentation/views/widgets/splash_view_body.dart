e41b35b504e655950fce73f331d2a6ae8d343493
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-11-14 21:50:35 +0200

    change theme

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,17 +1,24 @@
 import 'package:bookly_app/core/utils/assets.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AppAssets.logo),
-
+        const SizedBox(
+          height: 4,
+        ),
+        Text(
+          'Read Free Books',
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        )
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.primaryColor
-      ) ,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: AppColors.primaryColor,
+      ),
       home: const SplashView(),
     );
   }
