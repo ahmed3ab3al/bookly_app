2ef5694d312f30ac7069652fa88caec599f3b186
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-12-01 22:31:23 +0200

    finishing animation

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -8,26 +8,31 @@ class SplashViewBody extends StatefulWidget {
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
 
-class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
-
-
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
     animationController = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 2),
+      duration: const Duration(seconds: 3),
     );
 
-
     slideAnimation = Tween<Offset>(
-      begin: const Offset(0, 2),
+      begin: const Offset(0, 10),
       end: Offset.zero,
     ).animate(animationController);
     animationController.forward();
   }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
   late AnimationController animationController;
-  late Animation slideAnimation;
+  late Animation<Offset> slideAnimation;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -38,11 +43,17 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
         const SizedBox(
           height: 4,
         ),
-        Text(
-          'Read Free Books',
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
-        )
+        AnimatedBuilder(
+          animation: animationController,
+          builder: (context, child) => SlideTransition(
+            position: slideAnimation,
+            child: Text(
+              'Read Free Books',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
       ],
     );
   }
