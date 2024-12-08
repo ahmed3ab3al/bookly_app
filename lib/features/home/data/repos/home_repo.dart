644e296ef35ba09f4971cf60f6ee332009ab11a2
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-12-08 21:57:37 +0200

    handel failure and success condition in repo

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,7 +1,11 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/Book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo{
 
-  getHomeData();
-  getBestSellerData();
+ Future<Either<Failure,List<BookModel>>> getHomeData();
+ Future<Either<Failure,List<BookModel>>> getBestSellerData();
 
 
 
