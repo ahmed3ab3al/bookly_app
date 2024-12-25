6402d92948073a8f9b54d41833c5b9f64dc41838
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-12-25 20:33:37 +0200

    featured cubit states

--- lib/features/home/presentation/view_models/featured_books_cubit/featured_books_state.dart
+++ lib/features/home/presentation/view_models/featured_books_cubit/featured_books_state.dart
@@ -1,4 +1,20 @@
 
+import '../../../data/models/Book_model.dart';
+
 abstract class FeaturedBooksState {}
 
 class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksSuccess extends FeaturedBooksState {
+
+  final List<BookModel> books;
+
+  FeaturedBooksSuccess(this.books);
+}
+
+class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errMessage;
+
+  FeaturedBooksFailure(this.errMessage);
+}
+class FeaturedBooksLoading extends FeaturedBooksState {}
