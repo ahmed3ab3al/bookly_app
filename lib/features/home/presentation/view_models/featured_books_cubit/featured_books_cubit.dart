4f02b91a0fc14ed6b6b63cf4639f8105bf45751a
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-12-26 20:23:21 +0200

    featured books method

--- lib/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart
@@ -1,8 +1,21 @@
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/features/home/presentation/view_models/featured_books_cubit/featured_books_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
 
   static FeaturedBooksCubit get(context) => BlocProvider.of(context);
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.getHomeData();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.errorModel.message));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
