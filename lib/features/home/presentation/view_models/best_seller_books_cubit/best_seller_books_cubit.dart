152552cf809167a5cb013a287b04869669b49d5d
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-12-27 19:44:48 +0200

    best seller books method

--- lib/features/home/presentation/view_models/best_seller_books_cubit/best_seller_books_cubit.dart
+++ lib/features/home/presentation/view_models/best_seller_books_cubit/best_seller_books_cubit.dart
@@ -1,9 +1,22 @@
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/features/home/presentation/view_models/best_seller_books_cubit/best_seller_books_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BestSellerBooksCubit extends Cubit<BestSellerBooksState> {
-  BestSellerBooksCubit() : super(BestSellerBooksInitial());
+  BestSellerBooksCubit(this.homeRepo) : super(BestSellerBooksInitial());
 
   static BestSellerBooksCubit get(context) => BlocProvider.of(context);
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchBestSellerBooks() async {
+    emit(BestSellerBooksLoading());
+    var result = await homeRepo.getNewestData();
+    result.fold((failure) {
+      emit(BestSellerBooksFailure(failure.errorModel.message));
+    }, (books) {
+      emit(BestSellerBooksSuccess(books));
+    });
+  }
 
 }
--- lib/features/home/presentation/view_models/best_seller_books_cubit/best_seller_books_state.dart
+++ lib/features/home/presentation/view_models/best_seller_books_cubit/best_seller_books_state.dart
@@ -1,3 +1,14 @@
+import '../../../data/models/Book_model.dart';
+
 abstract class BestSellerBooksState {}
 
 class BestSellerBooksInitial extends BestSellerBooksState {}
+class BestSellerBooksLoading extends BestSellerBooksState {}
+class BestSellerBooksSuccess extends BestSellerBooksState {
+  final List<BookModel> books;
+  BestSellerBooksSuccess(this.books);
+}
+class BestSellerBooksFailure extends BestSellerBooksState {
+  final String errorMessage;
+  BestSellerBooksFailure(this.errorMessage);
+}
