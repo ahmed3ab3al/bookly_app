accc514ea62a199b20ff078b6986cfac30352312
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2024-12-28 21:43:39 +0200

    keep streak

--- lib/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart
@@ -11,6 +11,7 @@ class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
 
   Future<void> fetchFeaturedBooks() async {
     emit(FeaturedBooksLoading());
+
     var result = await homeRepo.getHomeData();
     result.fold((failure) {
       emit(FeaturedBooksFailure(failure.errorModel.message));
