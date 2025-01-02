4edd65fa7d8a92f36243f4bad7fcfbe9d16eb03b
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2025-01-02 22:25:35 +0200

    no change

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:bookly_app/core/api/dio_helper.dart';
 import 'package:bookly_app/core/utils/colors.dart';
 import 'package:bookly_app/core/utils/app_routers.dart';
 import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo_implement.dart';
 import 'package:bookly_app/features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
